4622311d7dcb253cb9c798fe941557a7168ff7f2
DESKTOP-C581OGQ\lockt <locktail14@gmail.com>
2026-06-11 13:52:47 +0900

    1. Move

--- Assets/__Project/Scripts/Runtime/FSM/Core/FSM.cs
+++ Assets/__Project/Scripts/Runtime/FSM/Core/FSM.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 [Serializable]
 public class FSM
 {
+    [SerializeField] private string m_subject = "";
     [SerializeField] private FSMStateSO m_startState = null;
+    [SerializeField, TextArea] private string m_note = "";
 
     private FSMCtrl m_ctrl;
     private FSMStateSO m_currentState = null;
